99ac63dbf78667bba59d93cac840e61e7205d16a
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-02-11 13:48:42 +0100

    Add resolver class

---
+++ Lox/Resolver.cs
@@ -0,0 +1,205 @@
+using Lox.Expressions;
+using Lox.Statements;
+
+namespace Lox;
+
+public class Resolver : Expressions.IVisitor<None?>, Statements.IVisitor<None?>
+{
+    private readonly Interpreter _interpreter;
+    private readonly Stack<Dictionary<string, bool>> _scopes = new();
+
+    public Resolver(Interpreter interpreter)
+    {
+        _interpreter = interpreter;
+    }
+
+    private void Resolve(List<Stmt> statements)
+        => statements.ForEach(Resolve);
+
+    private void Resolve(Stmt statement)
+        => statement.Accept(this);
+
+    private void Resolve(Expr expression)
+        => expression.Accept(this);
+
+    public None? Visit(Binary binary)
+    {
+        Resolve(binary.Left);
+        Resolve(binary.Right);
+        return null;
+    }
+
+    public None? Visit(Grouping grouping)
+    {
+        Resolve(grouping.Expression);
+        return null;
+    }
+
+    public None? Visit(Literal literal)
+        => null;
+
+    public None? Visit(Unary unary)
+    {
+        Resolve(unary.Right);
+        return null;
+    }
+
+    public None? Visit(Ternary ternary)
+    {
+        Resolve(ternary.Condition);
+        Resolve(ternary.IfTrue);
+        Resolve(ternary.IfFalse);
+        return null;
+    }
+
+    public None? Visit(Variable variable)
+    {
+        if (_scopes.Count > 0 && _scopes.Peek()[variable.Name.Lexeme] == false)
+            Program.Error(variable.Name, "Can't read local variable in its own initializer.");
+        
+        ResolveLocal(variable, variable.Name);
+        return null;
+    }
+
+    private void ResolveLocal(Expr expr, Token name)
+    {
+        Stack<Dictionary<string, bool>> tmp = new();
+        int i = -1;
+        while (_scopes.Count > 0)
+        {
+            i++;
+            var current = _scopes.Pop();
+            tmp.Push(current);
+            if (current.ContainsKey(name.Lexeme))
+            {
+                _interpreter.Resolve(expr, i);
+                break;
+            }
+        }
+        while (tmp.Count > 0)
+            _scopes.Push(tmp.Pop());
+    }
+
+    public None? Visit(Assign assign)
+    {
+        Resolve(assign.Value);
+        ResolveLocal(assign, assign.Name);
+        return null;
+    }
+
+    public None? Visit(Logical logical)
+    {
+        Resolve(logical.Left);
+        Resolve(logical.Right);
+        return null;
+    }
+
+    public None? Visit(Call call)
+    {
+        Resolve(call.Callee);
+        call.Arguments.ForEach(Resolve);
+        return null;
+    }
+
+    public None? Visit(Lambda lambda)
+    {
+        ResolveFunctionlike(lambda);
+        return null;
+    }
+
+    public None? Visit(Expression expression)
+    {
+        Resolve(expression.Expr);
+        return null;
+    }
+
+    public None? Visit(Print print)
+    {
+        Resolve(print.Expression);
+        return null;
+    }
+
+    public None? Visit(Var var)
+    {
+        Declare(var.Name);
+        if (var.Initializer != null)
+            Resolve(var.Initializer);
+        Define(var.Name);
+        return null;
+    }
+
+    private void Declare(Token name)
+    {
+        if (_scopes.Count <= 0)
+            return;
+        _scopes.Peek().Add(name.Lexeme, false);
+    }
+
+    private void Define(Token name)
+    {
+        if (_scopes.Count <= 0)
+            return;
+        _scopes.Peek()[name.Lexeme] = true;
+    }
+
+    public None? Visit(Block block)
+    {
+        BeginScope();
+        Resolve(block.Statements);
+        EndScope();
+        return null;
+    }
+
+    private void BeginScope()
+        => _scopes.Push(new Dictionary<string, bool>());
+
+    private void EndScope()
+        => _scopes.Pop();
+
+    public None? Visit(If @if)
+    {
+        Resolve(@if.Condition);
+        Resolve(@if.ThenBranch);
+        if (@if.ElseBranch != null)
+            Resolve(@if.ElseBranch);
+        return null;
+    }
+
+    public None? Visit(While @while)
+    {
+        Resolve(@while.Condition);
+        Resolve(@while.Body);
+        return null;
+    }
+
+    public None? Visit(Break @break)
+        => null;
+
+    public None? Visit(Function function)
+    {
+        Declare(function.Name);
+        Define(function.Name);
+        
+        ResolveFunctionlike(function);
+        return null;
+    }
+
+    private void ResolveFunctionlike(IFunctionlike functionlike)
+    {
+        BeginScope();
+        foreach (var param in functionlike.Params)
+        {
+            Declare(param);
+            Define(param);
+        }
+        Resolve(functionlike.Body);
+        EndScope();
+    }
+
+    public None? Visit(Return @return)
+    {
+        if (@return.Value != null)
+            Resolve(@return.Value);
+        return null;
+    }
+}
